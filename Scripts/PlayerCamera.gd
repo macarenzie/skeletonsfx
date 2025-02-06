a1ccc2677a289fa007c13656a0465143a9d7d426
phbr1768 <92740792+phbr1768@users.noreply.github.com>
2025-02-06 13:51:27 -0500

    player movement complete as of now

---
+++ Scripts/PlayerCamera.gd
@@ -0,0 +1,26 @@
+extends Camera3D
+
+@export var SENSITIVITY_X = 0.02
+@export var SENSITIVITY_Y = 0.02
+
+@onready var player = $"../.."
+@onready var camera = %PlayerCamera
+
+func _ready():
+	# capture and hide mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		#grab motion of mouse and rotate camera
+		player.rotate_y(-event.relative.x * SENSITIVITY_X)
+		camera.rotate_x(-event.relative.y * SENSITIVITY_Y)
+		#clamp the rotation so that it doesnt flip upside down
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	pass
+
