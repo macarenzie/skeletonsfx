20415a7bbd0c94196f57f1bbf7faa3a301b3921d
JamesTunney <89762439+JamesTunney@users.noreply.github.com>
2025-02-10 16:12:05 -0500

    Working Inventory
     Inventory is currently under the player, pauses and un-pauses with code in the world file. Use "i" to open and close, left click to place, right click to rotate.
    
    Also need to change most things process to pausable for things to work.

---
+++ Inventory/slot.gd
@@ -0,0 +1,39 @@
+extends TextureRect
+
+signal slot_entered(slot)
+signal slot_exited(slot)
+
+@onready var filter = $Status
+
+var slot_ID
+var is_hovering := false
+enum Status {DEAFAULT, TAKEN, FREE}
+var state := Status.DEAFAULT
+var item_stored = null
+
+#sets the color to the aproprate state
+func set_color(a_state = Status.DEAFAULT):
+	match a_state:
+		Status.DEAFAULT:
+			filter.color = Color(Color.WHITE, 0.0)
+		Status.TAKEN:
+			filter.color = Color(Color.RED, 0.2)
+		Status.FREE:
+			filter.color = Color(Color.GREEN, 0.2)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Keeps track of current slot the mouse is over
+	if get_global_rect().has_point(get_global_mouse_position()):
+		if not is_hovering:
+			is_hovering = true
+			emit_signal("slot_entered",self)
+		else:
+			if is_hovering:
+				is_hovering = false
+				emit_signal("slot_exited",self)
---
+++ Scripts/world.gd
@@ -0,0 +1,29 @@
+#Currently just used for inventory open/close/pause
+
+extends Node
+
+@onready var inventory = $Player/Inventory
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#close Inventory
+	inventory.close()
+	
+
+func _input(event):
+	#Helps togle the inventory.
+	if event.is_action_pressed("toggle_inventory"):
+		if inventory.isOpen:
+			inventory.close()
+		else:
+			inventory.open()
+
+#unpauses the game and sets mouse back to normal
+func _on_inventory_closed():
+	get_tree().paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
+
+#Pauses the game and sets mouse back to visable and stuck in window
+func _on_inventory_opened():
+	get_tree().paused = true
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED 
