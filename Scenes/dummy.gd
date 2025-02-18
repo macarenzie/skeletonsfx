70caf8d796a64d74706b74b0462a429a28c111d0
James Tunney (RIT Student) <jnt6801@rit.edu>
2025-02-18 12:50:16 -0500

    Less hello

--- Scenes/dummy.gd
+++ Scenes/dummy.gd
@@ -1,4 +1,4 @@
-extends Area3D
+extends StaticBody3D
 
 @export var material: String = "wood"
 @export var health: int = 500  # Use integer for health
@@ -8,15 +8,7 @@ func take_damage(amount: float):
 	health -= int(amount)
 	print("Dummy health: ", health)
 	if health <= 0:
-		print("DEAD")
-		health +=500  # Destroy when health hits 0
+		print("Dummy health: 0 \n YOU'VE KILLED THE DUMMY!")
+		health = 500 # Destroy when health hits 0
 func get_material() -> String:
 	return material 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-		print("Dummy health: ", health)
