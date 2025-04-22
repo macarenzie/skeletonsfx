ff9b5ae4739c83e0b00b2c6c78352a43ab6f598c
James Tunney (RIT Student) <jnt6801@rit.edu>
2025-04-22 16:46:55 -0400

    Animation Update.
    I was bored for a bit.

--- Scripts/cutscene.gd
+++ Scripts/cutscene.gd
@@ -3,12 +3,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("camera_animation")
+	$AnimationPlayer.play("Start_1")
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_key_pressed(KEY_Q):
+		$AnimationPlayer.stop()
+		$AnimationPlayer.play("Start_3")
 	pass
 
 
@@ -18,4 +21,9 @@ func _input(event):
 
 
 func _on_animation_player_animation_finished(anim_name):
-	get_tree().change_scene_to_file("res://Scenes/level_1_NEW.tscn")
+	if anim_name == "camera_animation":
+		get_tree().change_scene_to_file("res://Scenes/level_1_NEW.tscn")
+	if anim_name == "Start_1":
+		$AnimationPlayer.play("Start_2")
+	if anim_name == "Start_3":
+		$AnimationPlayer.play("camera_animation")
